980f0638daeeed92c448a62906b178fe10df5f50
RichardVong192 <53926483+RichardVong192@users.noreply.github.com>
2020-08-17 17:50:26 +1200

    Delete users.photos.routes.js

--- assignment1-server/app/routes/users.photos.routes.js
+++
@@ -1,8 +0,0 @@
-const photos = require('../controllers/users.photos.controller');
-
-module.exports = function (app) {
-    app.route(app.rootUrl + '/users/:user_id/photo')
-        .get(photos.getUsersPhoto)
-        .put(photos.putUsersPhoto)
-        .delete(photos.deleteUsersPhoto);
-};
