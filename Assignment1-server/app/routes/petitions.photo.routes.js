aa8b0ec8ec8c73d7902df0e8e5bf59f2488bc1c5
RichardVong192 <53926483+RichardVong192@users.noreply.github.com>
2020-08-22 17:51:55 +1200

    Delete petitions.photo.routes.js

--- Assignment1-server/app/routes/petitions.photo.routes.js
+++
@@ -1,7 +0,0 @@
-const photos = require('../controllers/petitions.photos.controller');
-
-module.exports = function (app) {
-    app.route(app.rootUrl + '/petitions/:petition_id/photo')
-        .get(photos.getPetitionsPhoto)
-        .put(photos.putPetitionsPhoto);
-};
